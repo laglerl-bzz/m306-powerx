835bb2c30be8e1e3c12727dddd15ff2c0ea05b57
laglerl-bzz <laglerl@bzz.ch>
2025-05-19 08:44:37 +0200

    new frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <nav className="bg-zinc-500 text-white flex items-center justify-between p-4">
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2"></div>
+          <img src="/logo.png" alt="Logo" className="h-10" />
+        </div>
+        <ul className="flex space-x-4 ">
+          <li>
+            <a href="#" className="hover:text-gray-400 flex items-center space-x-1 font-bold">
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-400 flex items-center space-x-1 font-bold">
+              <span>Chart</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-400 flex items-center space-x-1 font-bold">
+              <span>Upload</span>
+            </a>
+          </li>
+        </ul>
+      </nav >
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        {/* <Chart/> */}
+      </div>
+    </>
+  )
+}
+
+export default App

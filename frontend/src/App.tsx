3226a68ac52c2e2ecd54684981aa16699d6f5699
spken <134706997+spken@users.noreply.github.com>
2025-05-19 11:06:23 +0200

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,12 @@
 import { ChartComp } from "./components/chart"
 import { Card } from "./components/ui/card"
+import Navbar from "./components/ui/navbar"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select"
 
 function App() {
   return (
     <>
-      <nav className="bg-zinc-500 text-white flex items-center justify-between p-4">
-        <div className="flex items-center space-x-4">
-          <div className="flex items-center space-x-2"></div>
-          <img src="/logo.png" alt="Logo" className="h-10" />
-        </div>
-        <ul className="flex space-x-4 ">
-          <li>
-            <a href="#" className="hover:text-gray-400 flex items-center space-x-1 font-bold">
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-gray-400 flex items-center space-x-1 font-bold">
-              <span>Chart</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-gray-400 flex items-center space-x-1 font-bold">
-              <span>Upload</span>
-            </a>
-          </li>
-        </ul>
-      </nav >
+      <Navbar /> 
       <div className="h-screen justify-center flex items-center">
         <div className="w-10/12 flex flex-row gap-4">
           <ChartComp />
---
+++ frontend/src/components/ui/navbar.tsx
@@ -0,0 +1,36 @@
+import { Home, Upload } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <div className="mb-4">
+      <div className="flex h-16 items-center px-4 max-w-7xl mx-auto justify-between">
+        <div className="flex">
+          <a href="/" className="flex items-center space-x-2">
+            <div className="h-6 w-6 bg-primary rounded-sm flex items-center justify-center">
+              <span className="text-primary-foreground text-xs font-bold">PX</span>
+            </div>
+            <span className="font-semibold">PowerX</span>
+          </a>
+        </div>
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <a 
+            href="/" 
+            className="flex items-center text-sm font-medium transition-colors hover:text-primary"
+          >
+            <Home className="mr-2 h-4 w-4" />
+            Home
+          </a>
+          <a 
+            href="/upload" 
+            className="flex items-center text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+          >
+            <Upload className="mr-2 h-4 w-4" />
+            Upload
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

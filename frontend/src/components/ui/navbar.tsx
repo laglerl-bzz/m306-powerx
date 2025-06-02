ffe10c378f3651b2acca747b523ba06a20a46639
spken <134706997+spken@users.noreply.github.com>
2025-06-02 16:16:06 +0200

    Remove border nav

--- frontend/src/components/ui/navbar.tsx
+++ frontend/src/components/ui/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const location = useLocation();
   const { pathname } = location;
   return (
-    <div className="mb-4 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="mb-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-16 items-center px-4 max-w-7xl mx-auto justify-between">
         <div className="flex">
           <Link to="/" className="flex items-center space-x-2">
